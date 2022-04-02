dacabae63fb55d2e0da666b1c2c95b127b6bdfbe
ShauTsar <ShauTsar@GGOS>
2022-04-02 18:28:20 +0500

    Add project files.

---
+++ WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(126, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(126, 83);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(12, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Кому(почта) ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(6, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Логин(Ваша почта)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(12, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Пароль(Ваш)";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(99, 121);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.PasswordChar = '*';
+            this.textBox3.Size = new System.Drawing.Size(100, 23);
+            this.textBox3.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.Location = new System.Drawing.Point(550, 78);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 24);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Сдаться";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(366, 188);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(296, 239);
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.listBox1.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(12, 188);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(316, 214);
+            this.listBox1.TabIndex = 15;
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(287, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Загадать число";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(276, 52);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 24);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Отгадать число";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(425, 52);
+            this.textBox4.MaxLength = 4;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 23);
+            this.textBox4.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(425, 111);
+            this.textBox5.MaxLength = 4;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 23);
+            this.textBox5.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Label label1;
+        private Label label2;
+        private NumericUpDown numericUpDown1;
+        private NumericUpDown numericUpDown2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label3;
+        private TextBox textBox3;
+        private Button button3;
+        private PictureBox pictureBox1;
+        private ListBox listBox1;
+        private Button button1;
+        private Button button2;
+        private TextBox textBox4;
+        private TextBox textBox5;
+    }
+}
---
+++ WinFormsApp2/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using MailKit.Net.Pop3;
+using MimeKit;
+using System.Drawing;
+namespace WinFormsApp2
+
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Visible = false;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            MailAddress from = new MailAddress(textBox2.Text);
+            MailAddress to = new MailAddress(textBox1.Text);
+            MailMessage m = new MailMessage(from, to);
+            m.Subject = "Быки и коровы";
+            m.Body = textBox5.Text ;
+            m.IsBodyHtml = true;
+            SmtpClient smtp = new SmtpClient("smtp.mail.ru", 587);
+            smtp.Credentials = new NetworkCredential(textBox2.Text, textBox3.Text);
+            smtp.EnableSsl = true;
+            smtp.Send(m);
+            MessageBox.Show("Число отправлено");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (var client = new Pop3Client())
+            {        
+                client.Connect("pop.mail.ru", 995, true);
+                client.AuthenticationMechanisms.Remove("XOAUTH2");
+                client.Authenticate(textBox2.Text, textBox3.Text);
+                var message = client.GetMessage(client.Count-1);
+                client.Disconnect(true);
+                char [] array = message.HtmlBody.ToCharArray();
+                char [] array2 = textBox4.Text.ToCharArray();
+                int bools = 0;
+                for(int i = 0; i < 4; i++)
+                {
+                    if (array[i] == array2[i]) bools++;
+                    
+                }
+                int cows = 0;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    for (int j = 0; j < array2.Length; j++)
+                    {
+                        if (array[i] == array2[j])
+                            cows++;
+                    }
+
+                }
+                cows = cows - bools;
+                listBox1.Items.Add("Ваше число: " + textBox4.Text + ". В нем " + bools.ToString() +
+                    " быков и " + cows.ToString() + " коров.");
+                if (bools == 4)
+                {
+                    pictureBox1.Visible = true;
+                    MessageBox.Show("Молодчина", "Хорош");
+                }
+
+            }   }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            PictureBox pictureBox2 = new PictureBox();
+            pictureBox2.Size = new Size(960,580);
+            pictureBox2.Image = Image.FromFile("C:\\Users\\ShauTsar\\Downloads\\31Kl.gif");
+            pictureBox2.Location = new Point(0, 0);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            this.Controls.Add(pictureBox2);
+            MessageBox.Show("Nice try", "BB");
+            this.Close();
+        }
+
+        
+    }
+}
